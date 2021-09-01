99e99901f6f5385b37702024edd1ee4842d3b9b9
Ivan Elkin <elk.iv@yandex.ru>
2021-09-01 18:40:08 +0300

    Initial Commit

---
+++ PersonalityQuiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ResultViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Alexey Efimov on 30.08.2021.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    // 1. Передать сюда массив с ответами
+    // 2. Определить наиболее часто встречающийся тип животного
+    // 3. Отобразить результат в соответствии с этим животным
+    // 4. Избавиться от кнопки возврата на предыдущий экран
+    
+    var questions: [Question]!
+    var answersChosen: [Answer]!
+    var currentAnswers: [Answer] {
+        questions[questionIndex].answers
+    }
+    private var questionIndex = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.setHidesBackButton(true, animated: true)
+        print(answersChosen ?? 0)
+        print(answersChosen.count)
+    }
+    
+}
